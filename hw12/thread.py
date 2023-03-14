d9c26fc3fe475e3ef402eb33309af2b863275e2e
Volodymyr <nick919.ua@gmail.com>
2023-03-14 16:50:20 +0200

    fix: fixed bags

--- hw12/thread.py
+++ hw12/thread.py
@@ -6,12 +6,14 @@ import json
 cur_dict = {}
 
 def save_cache(name, data):
+    '''Function for saving json cache from API'''
     with open (name, 'w') as f: 
         json.dump(data, f)
         result = 'Cache saved to json file'
         return result
     
 def get_data(url, patch):
+    '''Function for get json data from API and raturning data'''
     try:
         req = requests.get(url)
         data = req.json()
@@ -24,6 +26,8 @@ def get_data(url, patch):
             return data
         
 def get_pryvat():
+    '''Function for get USD buy rate from Pryvatbank by API url. 
+    Returns usd/uah buy rate'''
     url = 'https://api.privatbank.ua/p24api/pubinfo?exchange&coursid=5'
     patch = fr'git_basics\hw12\data\pryvat_data.json'
     try:
@@ -36,6 +40,8 @@ def get_pryvat():
         print (f'{ex}')
 
 def get_nbu():
+    '''Function for get USD buy rate from NBU by API url. 
+    Returns usd/uah buy rate'''
     url = 'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json'
     patch = fr'git_basics\hw12\data\nbu_data.json'
     try: 
@@ -48,23 +54,25 @@ def get_nbu():
         print (f'{ex}')
 
 def get_exim():
+    '''Function for get USD buy rate from EXIM BANK by API url. 
+    Returns usd/uah buy rate'''
     url ='https://www.eximb.com/services/v1/rates/?lang=uk'
     patch = fr'git_basics\hw12\data\exim_data.json'
     try:
         data = get_data(url, patch)
         usd = float(data['rates']['cash']['data'][0]['buy'].replace(',', '.'))
         cur_dict['EXIM-BANK'] = usd
-        print(usd)
         return usd
     except Exception as ex:
         print (f'{ex}')
 
 def currency(cur_dict):
-        min_value = max(cur_dict.values())
-        min_key = max(cur_dict, key=cur_dict.get)
+        '''Function returns best buy rate for USD/UAH from dict'''
+        m_value = max(cur_dict.values())
+        m_key = max(cur_dict, key=cur_dict.get)
         print(cur_dict)
-        print(f'The best USD/UAH buy rate is: {min_key}, {min_value}')
-        return f'The best bay USD/UAH buy rate is: {min_key}, {min_value}'
+        print(f'The best USD/UAH buy rate is: {m_key}, {m_value} uah for 1 dollar')
+        return f'The best bay USD/UAH buy rate is: {m_key}, {m_value} uah for 1 dollar'
 
 get1 = threading.Thread(target=get_pryvat, name='Pryvat Bank API thread')
 get2 = threading.Thread(target=get_nbu, name='NBU API thread')
